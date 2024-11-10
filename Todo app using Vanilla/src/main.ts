39d102ae930f39949324ae78229360ff6530012a
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-11-11 03:59:57 +0530

    TS(#1)

---
+++ Todo app using Vanilla/src/main.ts
@@ -0,0 +1,73 @@
+import './style.css'
+
+interface Todo {
+  title: string;
+  isCompleted: boolean;
+  readonly id: string;
+}
+
+const todos: Todo[] = []
+const todoContainer = document.querySelector(".todoContainer") as HTMLDivElement
+const todoInput = document.getElementsByName("title")[0] as HTMLInputElement
+const myForm = document.getElementById("myForm") as HTMLFormElement
+
+myForm.onsubmit = (e: SubmitEvent) => {
+  e.preventDefault() // by adding this, onSubmit it won't refresh the page
+
+  const todo: Todo = {
+    title: todoInput.value,
+    isCompleted: false,
+    id: String(Math.random() * 1000)
+  }
+
+  todos.push(todo)
+  todoInput.value = ""
+  renderTodo(todos)
+}
+
+const generateTodoItem = (title: string, isCompleted: boolean, id: string) => {
+  const todo: HTMLDivElement = document.createElement("div")
+  todo.className = "todo"
+
+  // checkbox
+  const checkBox: HTMLInputElement = document.createElement("input")
+  checkBox.setAttribute("type", "checkbox")
+  checkBox.className = "isCompleted"
+  checkBox.checked = isCompleted
+  checkBox.onchange = () => {
+    todos.find(item => {
+      if(item.id === id) item.isCompleted = checkBox.checked
+    })
+    paragraph.className = checkBox.checked ? "textCut" : ""
+  }
+
+  // title
+  const paragraph: HTMLParagraphElement = document.createElement("p")
+  paragraph.innerText = title
+  paragraph.className = isCompleted ? "textCut" : ""
+
+  // delete button
+  const btn: HTMLButtonElement = document.createElement("button")
+  btn.innerText = 'x'
+  btn.className = "deleteBtn"
+  btn.onclick = () => {
+    deleteTodo(id)
+  }
+
+  // appending all to todoItem
+  todo.append(checkBox, paragraph, btn)
+  todoContainer.append(todo)
+}
+
+const deleteTodo = (id: string) => {
+  const idx = todos.findIndex(item => item.id === id)
+  todos.splice(idx, 1)
+  renderTodo(todos)
+}
+
+const renderTodo = (todos: Todo[]) => {
+  todoContainer.innerText = ""
+  todos.forEach(item => {
+    generateTodoItem(item.title, item.isCompleted, item.id)
+  })
+}
